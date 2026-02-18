d02a92677bafb70b9692e329b716dee9f85f54c0
MireauLijbaert <mireau.lijbaert@student.howest.be>
2026-02-18 22:55:41 +0100

    Forgot to add update and render for components in GameObject

--- Minigin/GameObject.cpp
+++ Minigin/GameObject.cpp
@@ -7,12 +7,29 @@ dae::GameObject::~GameObject() = default;
 
 void dae::GameObject::Update()
 {
+	for (const auto& component : m_components)
+	{
+		component->Update();
+	}
+	// Remove components that were marked for removal
+	for (const auto& component : m_componentsToRemove)
+	{
+		m_components.erase(std::remove_if(m_components.begin(), m_components.end(),
+			[component](const std::unique_ptr<BaseComponent>& ptr) { return ptr.get() == component; }),
+			m_components.end());
+	}
+	m_componentsToRemove.clear();
 }
 
 void dae::GameObject::Render() const
 {
 	const auto& pos = m_transform.GetPosition();
 	Renderer::GetInstance().RenderTexture(*m_texture, pos.x, pos.y);
+
+	for (const auto& component : m_components)
+	{
+		component->Render();
+	}
 }
 
 void dae::GameObject::SetTexture(const std::string& filename)
