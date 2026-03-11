55ab5c24a6b42fa42118297b6509847ab40937f7
MireauLijbaert <mireau.lijbaert@student.howest.be>
2026-03-11 18:49:56 +0100

    -CmakeLists Added the Xinput library
    -InputManager
    Added logic to bind commands to up/down/held key on the gamepad and keyboard, pimpl'ed away

--- Minigin/InputManager.cpp
+++ Minigin/InputManager.cpp
@@ -1,25 +1,190 @@
+// Done in minigin too but things don't work here if it's not in here directly
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+#include "Xinput.h"
+
 #include <SDL3/SDL.h>
 #include <backends/imgui_impl_sdl3.h>
+
 #include "InputManager.h"
+
+#include <unordered_map>
+
+
+class dae::InputManager::impl
+{
+public:
+	bool ProcessInput() 
+	{
+		// Keyboard DOWN and UP events
+		SDL_Event e;
+		while (SDL_PollEvent(&e))
+		{
+			if (e.type == SDL_EVENT_QUIT) {
+				return false;
+			}
+			if (e.type == SDL_EVENT_KEY_DOWN) {
+
+				for (const auto& binding : m_keyboardCommandsDown)
+				{
+					if (e.key.scancode == binding.first)
+					{
+						binding.second->Execute();
+					}
+				}
+			}
+			if (e.type == SDL_EVENT_KEY_UP) {
+
+				for (const auto& binding : m_keyboardCommandsUp)
+				{
+					if (e.key.scancode == binding.first)
+					{
+						binding.second->Execute();
+					}
+				}
+			}
+
+			//process event for IMGUI
+			ImGui_ImplSDL3_ProcessEvent(&e);
+		}
+
+		// Keyboard HELD events (uses a different method)
+		const bool* keyboardState = SDL_GetKeyboardState(nullptr);
+
+		for (const auto& binding : m_keyboardCommandsHeld)
+		{
+			if (keyboardState[binding.first])
+			{
+				binding.second->Execute();
+			}
+		}
+
+		// Gamepad
+		DWORD dwResult;
+		// loop through all possible controller slots (0 to XUSER_MAX_COUNT - 1) XUSER_MAX_COUNT is defined in Xinput.h as 4, meaning we can support up to 4 controllers (though could change this to 2 for exam project)
+		for (DWORD i = 0; i < XUSER_MAX_COUNT; i++)
+		{
+			XINPUT_STATE state{};
+			dwResult = XInputGetState(i, &state);
+			if (dwResult == ERROR_SUCCESS)
+			{
+				WORD buttonState = state.Gamepad.wButtons;
+				WORD previousButtonState = m_previousButtons[i];
+
+				// HELD
+				for (const auto& binding : m_gamepadCommandsHeld)
+				{
+					if (buttonState & binding.first)
+					{
+						binding.second->Execute();
+					}
+				}
+
+				// DOWN
+
+				for (const auto& binding : m_gamepadCommandsDown)
+				{
+					bool isPressed = (buttonState & binding.first) != 0; // !=0 checks if the button is currently pressed (if bit is 0 then it's not pressed)
+					bool wasPressed = (previousButtonState & binding.first) != 0;
+
+					if (!wasPressed && isPressed) // if it is pressed but wasn't before
+					{
+						binding.second->Execute();
+					}
+				}
+
+				// UP
+				for (const auto& binding : m_gamepadCommandsUp)
+				{
+					bool isPressed = (buttonState & binding.first) != 0; // !=0 checks if the button is currently pressed (if bit is 0 then it's not pressed)
+					bool wasPressed = (previousButtonState & binding.first) != 0;
+
+					if (wasPressed && !isPressed) // if it was pressed but isn't now
+					{
+						binding.second->Execute();
+					}
+				}
+
+				// Update the previous button state for the next frame
+				m_previousButtons[i] = buttonState;
+			}
+			else
+			{
+				// Controller is not connected, set it's state to 0 for if it was a previously connected
+				m_previousButtons[i] = 0;
+			}
+		}
+
+		return true;
+	}
+
+	void BindKeyboardInput(SDL_Scancode key, std::unique_ptr<Command> command, InputState inputState)
+	{
+		if (inputState == InputState::Up)
+		{
+			m_keyboardCommandsUp[key] = std::move(command);
+		}
+		else if (inputState == InputState::Down)
+		{
+			m_keyboardCommandsDown[key] = std::move(command);
+		}
+		else if (inputState == InputState::Held)
+		{
+			m_keyboardCommandsHeld[key] = std::move(command);
+		}
+	}
+
+	// Use WORD here, the uint16_t automatically conversts to WORD, no casting needed
+	void BindGamepadInput(WORD button, std::unique_ptr<Command> command, InputState inputState)
+	{
+		if (inputState == InputState::Up)
+		{
+			m_gamepadCommandsUp[button] = std::move(command);
+		}
+		else if (inputState == InputState::Down)
+		{
+			m_gamepadCommandsDown[button] = std::move(command);
+		}
+		else if (inputState == InputState::Held)
+		{
+			m_gamepadCommandsHeld[button] = std::move(command);
+		}
+	}
+
+
+private:
+	// 3 different states for keyboard (maybe change this so a vector
+	std::unordered_map<SDL_Scancode, std::unique_ptr<Command>> m_keyboardCommandsUp{};
+	std::unordered_map<SDL_Scancode, std::unique_ptr<Command>> m_keyboardCommandsDown{};
+	std::unordered_map<SDL_Scancode, std::unique_ptr<Command>> m_keyboardCommandsHeld{};
+	
+	// 3 different states for gamepad
+	std::unordered_map<WORD, std::unique_ptr<Command>> m_gamepadCommandsUp{};
+	std::unordered_map<WORD, std::unique_ptr<Command>> m_gamepadCommandsDown{};
+	std::unordered_map<WORD, std::unique_ptr<Command>> m_gamepadCommandsHeld{};
+
+	// Remember the previous button states to be able to determine if a button was just pressed, just released or is being held down
+	WORD m_previousButtons[XUSER_MAX_COUNT]{};
+
+};
+
+dae::InputManager::InputManager()
+	: pImpl(std::make_unique<impl>())
+{
+}
+dae::InputManager::~InputManager() = default;
 
 bool dae::InputManager::ProcessInput()
 {
-	SDL_Event e;
-	while (SDL_PollEvent(&e)) {
-		if (e.type == SDL_EVENT_QUIT) {
-			return false;
-		}
-		if (e.type == SDL_EVENT_KEY_DOWN) {
-			
-		}
-		if (e.type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
-			
-		}
-		// etc...
+	return pImpl->ProcessInput();
+}
 
-		//process event for IMGUI
-		ImGui_ImplSDL3_ProcessEvent(&e);
-	}
+void dae::InputManager::BindKeyboardInput(SDL_Scancode key, std::unique_ptr<Command> command, InputState inputState)
+{
+	pImpl->BindKeyboardInput(key, std::move(command), inputState);
+}
 
-	return true;
+void dae::InputManager::BindGamepadInput(uint16_t button, std::unique_ptr<Command> command, InputState inputState)
+{
+	pImpl->BindGamepadInput(button, std::move(command), inputState);
 }
--- Minigin/InputManager.h
+++ Minigin/InputManager.h
@@ -1,12 +1,35 @@
 #pragma once
 #include "Singleton.h"
+#include "memory"
+#include "Commands.h"
+
+#include <cstdint>
+#include <SDL3/SDL.h>
 
 namespace dae
 {
+	enum class InputState
+	{
+		Up,
+		Down,
+		Held
+	};
+
 	class InputManager final : public Singleton<InputManager>
 	{
 	public:
+		InputManager();
+		~InputManager();
+
 		bool ProcessInput();
+
+		void BindKeyboardInput(SDL_Scancode key, std::unique_ptr<Command> command, InputState inputState );
+		// Use uint16_t instead of WORD to avoid including windows.h in this header, will convert it to WORD in the cpp file when needed
+		void BindGamepadInput(uint16_t button, std::unique_ptr<Command> command, InputState inputState);
+
+	private:
+		class impl;
+		std::unique_ptr<impl> pImpl;
 	};
 
 }
